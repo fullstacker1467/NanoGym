5c42b9b59d67f3f80733be5311f3bb3a1f26be6f
suhrob <suhrobrahmatulayev973132@gmail.com>
2023-05-10 22:39:15 +0500

    card yasaldi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const humb = document.querySelector(".humb");
 const ul = document.querySelector("#ulmenu");
 const body = document.querySelector("body");
 const nav = document.querySelector("nav");
+const bottom = document.querySelector(".third .container .bottom");
 
 humb.addEventListener("click", () => {
   body.classList.toggle("active");
@@ -23,4 +24,36 @@ window.addEventListener("scroll", () => {
   } else {
     nav.classList.remove("active");
   }
+});
+
+let cardData = [
+  {
+    icon: "fa-solid fa-dumbbell",
+    title: "QUALITY EQUIPMENT",
+    description:
+      "The sea freight service has grown consider ably in recent years. We spend timetting to kn.",
+  },
+  {
+    icon: "fa-solid fa-heart-pulse",
+    title: "HEALTH CARING",
+    description:
+      "The sea freight service has grown consider ably in recent years. We spend timetting to kn.",
+  },
+  {
+    icon: "fa-solid fa-stopwatch",
+    title: "GYM STRATEGIES",
+    description:
+      "The sea freight service has grown consider ably in recent years. We spend timetting to kn.",
+  },
+];
+
+cardData.forEach((item) => {
+  bottom.innerHTML += `
+  <div class="card" data-aos="fade-up">
+  <i class="${item.icon}"></i>
+  <h3>${item.title}</h3>
+  <p>${item.description}</p>
+  <a href="html/service.html">DISCOVER MORE ABOUT US <i class="fa-solid fa-arrow-right"></i></a>
+</div>
+    `;
 });
